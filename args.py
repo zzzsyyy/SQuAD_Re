25f77f1494da10c068ac30e6cde6763c378d2a61
zzzsy <zsy3141592@163.com>
2021-04-01 10:31:39 +0800

    update: update args

--- args.py
+++ args.py
@@ -7,7 +7,22 @@ def get_train_args():
     add_common_args(parser)
     add_train_test_args(parser)
 
-    
+    parser.add_argument('--eval_steps',
+                        type=int,
+                        default=50000,
+                        help='Number of steps between successive evaluations.')
+    parser.add_argument('--lr',
+                        type=float,
+                        default=0.5,
+                        help='Learning rate')
+    '''parser.add_argument('--l2_wd',
+                        type=float,
+                        default=0,
+                        help='L2 weight decay')'''
+    parser.add_argument('--num_epochs',
+                        type=int,
+                        default=30,
+                        help='Number of epoch for which to train.')
 
 
 def add_comme_args(parser):
@@ -21,3 +36,5 @@ def add_comme_args(parser):
                         type=str,
                         default='--./data/test')
     
+
+# def add_
